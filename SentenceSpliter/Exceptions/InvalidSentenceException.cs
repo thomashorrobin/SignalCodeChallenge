40fb2c3ec97e98f4cde66da30ae9edee4c2e06e3
thomashorrobin <thomasroberthorrobin@gmail.com>
2015-06-27 22:27:49 +1200

    fixed Sentence exception
    Sentence exception didn't inherart exception

--- SentenceSpliter/Exceptions/InvalidSentenceException.cs
+++ SentenceSpliter/Exceptions/InvalidSentenceException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SentenceSpliter.Exceptions
 {
-    class InvalidSentenceException
+    class InvalidSentenceException : Exception
     {
         public string SentenceText { get; set; }
 
