f48a6ff2edefbf5ab71b6aa7c77c8c183aea59c8
thomashorrobin <thomasroberthorrobin@gmail.com>
2015-06-27 18:53:21 +1200

    Added two more tests to the new test project

---
+++ SentenceSpliter.Tests/SentenceTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SentenceSpliter.Tests
+{
+    [TestClass]
+    public class SentenceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SentenceSpliter.Tests/TextTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SentenceSpliter.Tests
+{
+    [TestClass]
+    public class TextTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
