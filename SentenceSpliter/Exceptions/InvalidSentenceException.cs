8b3b0f160598e6020dee337a932ba9f71e81cace
thomashorrobin <thomasroberthorrobin@gmail.com>
2015-06-27 21:52:03 +1200

    Added some exceptions

---
+++ SentenceSpliter/Exceptions/InvalidSentenceException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceSpliter.Exceptions
+{
+    class InvalidSentenceException
+    {
+        public string SentenceText { get; set; }
+
+        public InvalidSentenceException(string sentenceText)
+        {
+            SentenceText = sentenceText;
+        }
+    }
+}
---
+++ SentenceSpliter/Exceptions/InvalidWordException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentenceSpliter.Exceptions
+{
+    public class InvalidWordException : Exception
+    {
+        public string WordText { get; set; }
+        public string SentenceText { get; set; }
+
+        public InvalidWordException(string wordText)
+        {
+            WordText = wordText;
+        }
+    }
+}
