97d27934e804cba5642efbceab32bc6f367e507b
thomashorrobin <thomasroberthorrobin@gmail.com>
2015-06-27 18:11:34 +1200

    Added new test project and deleted old one

---
+++ SentenceSpliter.Tests/WordTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SentenceSpliter.Tests
+{
+    [TestClass]
+    public class WordUnitTest
+    {
+        [TestMethod]
+        public void TestWord()
+        {
+        }
+    }
+}
