546e2331b05227ab520a0eaf6583c1deee1c9c97
thomashorrobin <thomasroberthorrobin@gmail.com>
2015-06-28 23:25:44 +1200

    fixed up the MVC side

--- SignalCodeChallenge/Controllers/HomeController.cs
+++ SignalCodeChallenge/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SentenceSpliter;
 
 namespace SignalCodeChallenge.Controllers
 {
@@ -13,18 +14,37 @@ namespace SignalCodeChallenge.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //[HttpPost]
+        //public ActionResult Index(string text)
+        //{
+        //    return Results(new Text(text));
+        //}
 
-            return View();
+        [HttpPost]
+        public ActionResult Results(string text)
+        {
+            return View(new Models.TextResult(new Text(text)));
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult FromFile(HttpPostedFileBase file)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            string text = "The default sentence";
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                //// extract only the fielname
+                //var fileName = Path.GetFileName(file.FileName);
+                //// store the file inside ~/App_Data/uploads folder
+                //var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                //file.SaveAs(path);
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(file.InputStream))
+                {
+                    text = reader.ReadToEnd();
+                }
+            }
+            // redirect back to the index action to show the form once again
+            return View("Results", new Models.TextResult(new Text(text)));
         }
     }
 }
